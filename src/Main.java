b14e2870b4dbcf3818d5683f34cffc1e4d8d4706
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2020-02-17 19:14:57 -0500

    Changed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Enter String here:");
-        Scanner scan = new Scanner(System.in);
-        String word = scan.nextLine();
+        String word = "TestPro";
         int count = word.length();
 
         if (count % 2 == 0) {
