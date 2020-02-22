f11b3396ed3c8a20dbfdf2a86cc2c239464a5ca7
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2020-02-22 01:36:45 -0500

    Week 3 Homework 2 – OOP Exercises.
    Week 3 Homework 2 – OOP Exercises.

---
+++ src/Car.java
@@ -0,0 +1,28 @@
+public class Car {
+
+     private String CountryOfCar;
+     private String Model;
+     private String Maker;
+
+     public void setCountryOfCar(String a){
+         CountryOfCar=a;
+     }
+     public void setModel(String b){
+         Model = b;
+     }
+     public void setMaker(String c){
+         Maker = c;
+     }
+
+    public String getCountryOfCar() {
+        return CountryOfCar;
+    }
+    public String getModel() {
+        return Model;
+    }
+    public String getMaker() {
+        return Maker;
+    }
+
+
+}
