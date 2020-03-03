c68621f4f7f4e01c18ae8124e60dfc3ad7cbcfe6
bujorgheorghe <56941435+bujorgheorghe@users.noreply.github.com>
2020-03-03 00:49:05 -0500

    Fixed homework SETS

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         //1.Initiate a Set. Using For loop add integers from 1 to 10.
         Set<Integer> test = new HashSet<>();
         for (int i = 1; i < 11; i++) {
@@ -11,32 +12,36 @@ public class Main {
         }
         System.out.println(test);
 
+
         //2.Remove all odd numbers from sets.
         int m = 0;
-        List<Integer> convert = new ArrayList<>(test);
-
+        Set<Integer> convert = new HashSet<>(test);
         for (int j = 0; j < convert.size(); j++) {
-            if (convert.get(j) % 2 != 0) {
-                convert.remove(j);
-                j -= m;
-                m++;
+            java.util.Iterator<Integer> itr = convert.iterator();
+            while (itr.hasNext()) {
+                int val = itr.next();
+                if (val % 9 == 0 || val % 2 != 0) {
+                    itr.remove();
+                }
             }
         }
-        test.clear();
-        test.addAll(convert);
-        System.out.println(test);
+        System.out.println(convert);
 
 
-        //3.Try to create a set with duplicate values.
-        Set<Integer> duplicate = new HashSet<>();
-        duplicate.add(8);
-        duplicate.add(3);
-        duplicate.add(8);
-        duplicate.add(2);
-        duplicate.add(3);
-        duplicate.add(101);
+                    //3.Try to create a set with duplicate values.
+                    Set<Integer> duplicate = new HashSet<>();
+                    duplicate.add(8);
+                    duplicate.add(3);
+                    duplicate.add(8);
+                    duplicate.add(2);
+                    duplicate.add(3);
+                    duplicate.add(101);
 
-        System.out.println(duplicate);
+                    System.out.println(duplicate);
+
+
+
     }
-}
 
+
+}
